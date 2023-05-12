0ce0d1d99ef634c16644d651505a2e67b27727e9
SimpleImpact <109104323+SimpleImpact@users.noreply.github.com>
2023-05-12 05:40:52 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import math
+import math, time
 
 count = 1
 page = 1
@@ -6,80 +6,98 @@ page = 1
 def is_whole(n):
         return n % 1 == 0
 
-print("1. Add         ")
-print("2. Subtract    ")
-print("3. Multiply    ")
-print("4. Divide      ")
-print("5. Factor      ")
-print("6. Square      ")
-print("7. Square Root ")
-print("8. Quadratic   ")
-print("9. Next Page")
-choice = int(input("Choose 1-9: "))
+def calc():
 
-if page == 1:
-    if choice == 9:
-        page = 2
+    global count
+    global page
 
-    if choice == 1:
-        print(int(input("1st Number: ")) + int(input("2nd Number: ")))
+    print("1. Add         ")
+    print("2. Subtract    ")
+    print("3. Multiply    ")
+    print("4. Divide      ")
+    print("5. Factor      ")
+    print("6. Square      ")
+    print("7. Square Root ")
+    print("8. Quadratic   ")
+    print("9. Next Page")
+    choice = int(input("Choose 1-9: "))
 
-    if choice == 2:
-        print(int(input("1st Number: ")) - int(input("2nd Number: ")))
+    if page == 1:
 
-    if choice == 3:
-        print(int(input("1st Number: ")) * int(input("2nd Number: ")))
+        if choice == 9:
+            page = 2
 
-    if choice == 4:
-        print(int(input("1st Number: ")) / int(input("2nd Number: ")))
+        if choice == 1:
+            print(int(input("1st Number: ")) + int(input("2nd Number: ")))
 
-    if choice == 5:
-        num = int(input("What number: "))
+        if choice == 2:
+            print(int(input("1st Number: ")) - int(input("2nd Number: ")))
 
-        while count <= num:
-            output = num / count
-            if is_whole(output):
-                print(int(output), end=" ")
+        if choice == 3:
+            print(int(input("1st Number: ")) * int(input("2nd Number: ")))
 
-            count += 1
+        if choice == 4:
+            print(int(input("1st Number: ")) / int(input("2nd Number: ")))
 
-    if choice == 6:
-        num = int(input("What number: "))
-        print(num * num)
+        if choice == 5:
+            num = int(input("What number: "))
 
-    if choice == 7:
-        num = int(input("What number: "))
-        print(int(math.sqrt(float(num))))
+            while count <= num:
+                output = num / count
+                if is_whole(output):
+                    print(int(output), end=" ")
 
-    if choice == 8:
-        a = float(input("a = "))
-        b = float(input("b = "))
-        c = float(input("c = "))
-        print(( -(b) + math.sqrt((b * b)) -4 * (a) * (c)) / a)
-        print(( -(b) - math.sqrt((b * b)) -4 * (a) * (c)) / a)
-            
-if page == 2:
-    print("\n", " Page 2", "\n")
-    print("1. Pythagorean   ")
-    print("2. Find Midpoint ")
-    choice = int(input("Choose 1-2: "))
+                count += 1
 
-    if choice == 1:
-        a = float(input("a = "))
-        b = float(input("b = "))
-        answer = (math.sqrt((a * a) + (b * b)))
-        if is_whole(answer):
-            print(answer)
-        else:
-            answer = answer * answer
-            print("sqrt(" + str(answer) + ")")
-    
-    if choice == 2:
-        x1 = int(input("What is x1: "))
-        x2 = int(input("What is x1: "))
-        y1 = int(input("What is x1: "))
-        y2 = int(input("What is x1: "))
-        x = (x1 + x2) / 2
-        y = (y1 + y2) / 2
-        print("(" + str(int(x)) + "," + str(int(y)) + ")")
+        if choice == 6:
+            num = int(input("What number: "))
+            print(num * num)
 
+        if choice == 7:
+            num = int(input("What number: "))
+            print(int(math.sqrt(float(num))))
+
+        if choice == 8:
+            a = float(input("a = "))
+            b = float(input("b = "))
+            c = float(input("c = "))
+            print(( -(b) + math.sqrt((b * b)) -4 * (a) * (c)) / a)
+            print(( -(b) - math.sqrt((b * b)) -4 * (a) * (c)) / a)
+
+    if page == 2:
+        print("\n", " Page 2", "\n")
+        print("1. Pythagorean (solve for c)  ")
+        print("2. Pythagorean (solve for a) ")
+        print("3. Find Midpoint ")
+        choice = int(input("Choose 1-2: "))
+
+        if choice == 1:
+            a = float(input("a = "))
+            b = float(input("b = "))
+            answer = (math.sqrt((a * a) + (b * b)))
+            if is_whole(answer):
+                print(answer)
+            else:
+                answer = answer * answer
+                print("sqrt(" + str(answer) + ")")
+
+        if choice == 2:
+            b = float(input("b = "))
+            c = float(input("c = "))
+            answer = math.sqrt((c * c) - (b * b))
+            if is_whole(answer):
+                print(answer)
+            else:
+                print("sqrt(" + str(int(answer * answer)) + ")")
+
+        if choice == 3:
+            x1 = int(input("What is x1: "))
+            x2 = int(input("What is x2: "))
+            y1 = int(input("What is y1: "))
+            y2 = int(input("What is y2: "))
+            x = (x1 + x2) / 2
+            y = (y1 + y2) / 2
+            print("(" + str(int(x)) + "," + str(int(y)) + ")")
+    time.sleep(3)
+    calc()
+calc()
